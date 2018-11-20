8bad7e81c575b4f095e316b03e95ca2da0d8e260
Kaloyan Stankov <45176113+kaloyans@users.noreply.github.com>
2018-11-20 19:10:10 +0200

    Create 3EqualNumbers.cs

---
+++ 3EqualNumbers.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            if (a == b && b == c)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+
+    }
+    
+}
